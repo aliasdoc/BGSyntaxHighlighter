d5c3bbd9ccb65f71b0ef2cbed0971056fd0105a7
KAZUMA Ukyo <yaakaito@gmail.com>
2012-08-07 14:40:44 +0900

    Initial Commit

---
+++ BGSyntaxHighlighter/BGSyntaxHighlighter.h
@@ -0,0 +1,13 @@
+//
+//  BGSyntaxHighlighter.h
+//  BGSyntaxHighlighter
+//
+//  Created by KAZUMA Ukyo on 12/08/07.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BGSyntaxHighlighter : NSObject
+
+@end
