f2ff9cc9aa4c3e5f83023e6bfe929b9541c6a116
KAZUMA Ukyo <yaakaito@gmail.com>
2012-08-10 12:07:36 +0900

    行数表示は分ける

---
+++ BGSyntaxHighlighter/BGSyntaxHighlightLineNumberView.h
@@ -0,0 +1,13 @@
+//
+//  BGSyntaxHighlightLineNumberView.h
+//  BGSyntaxHighlighter
+//
+//  Created by KAZUMA Ukyo on 12/08/10.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BGSyntaxHighlightLineNumberView : UIView
+
+@end
---
+++ BGSyntaxHighlighter/BGSyntaxHighlightLineNumberView.m
@@ -0,0 +1,31 @@
+//
+//  BGSyntaxHighlightLineNumberView.m
+//  BGSyntaxHighlighter
+//
+//  Created by KAZUMA Ukyo on 12/08/10.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "BGSyntaxHighlightLineNumberView.h"
+
+@implementation BGSyntaxHighlightLineNumberView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
